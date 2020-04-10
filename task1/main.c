91467a0aa1721ed76ab8e1e2aac25863a2a8a24f
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-10 22:09:25 +0200

    Calculate partner id

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,14 @@
 #include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <assert.h>
 
+// Calculate greates common dividor of a and b
 int nwd(int a, int b);
+
+// Simple implementation of accurate exponentiation
+int simplePow(int number, int exponent);
 
 int main(int argc, char **argv)
 {
@@ -14,7 +20,23 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    printf("%d\n", nwd(15, 57));
+    // TODO: Read numbers from args
+    // For now use fixed values
+    const int values[] = {15, 21, 24, 57};
+    assert(world_size == 4);
+
+    const int totalSteps = log2(world_size);
+    if (world_rank == 0)
+        printf("Calculations will take %d steps\n\n", totalSteps);
+
+    int myValue = values[world_rank];
+    for (int step = 0; step < totalSteps; step++)
+    {
+        const int partnerRank = (world_rank + simplePow(2, step)) % world_size;
+
+        printf("Itearton %d -> Process %d sends to %d\n", step, world_rank, partnerRank);
+    }
+
 
     MPI_Finalize();
     return EXIT_SUCCESS;
@@ -38,4 +60,13 @@ int nwd(int a, int b)
     }
 
     return a;
+}
+
+int simplePow(int number, int exponent)
+{
+    int result = 1;
+    while (exponent--)
+        result *= number;
+
+    return result;
 }
